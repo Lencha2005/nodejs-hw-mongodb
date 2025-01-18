1e1a11f26e94359e694e999368bd761f374c98d6
Lena_Bondar <lencha.2005@gmail.com>
2025-01-18 16:07:42 +0200

    googleOauth and swagger add

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,60 +1,69 @@
-import { THIRTY_DAYS } from "../constants/index.js";
-import { loginUser, logoutUser, refreshSession, registerUser, requestResetToken, resetPassword } from "../services/auth.js";
+import { THIRTY_DAYS } from '../constants/index.js';
+import {
+  loginOrSignupWithGoogle,
+  loginUser,
+  logoutUser,
+  refreshSession,
+  registerUser,
+  requestResetToken,
+  resetPassword,
+} from '../services/auth.js';
+import { generateAuthUrl } from '../utils/googleOAuth2.js';
 
 const setupSession = (res, session) => {
-    res.cookie('refreshToken', session.refreshToken, {
-        httpOnly: true,
-        expires: new Date(Date.now() + THIRTY_DAYS),
-      });
-      res.cookie('sessionId', session._id, {
-        httpOnly: true,
-        expires: new Date(Date.now() + THIRTY_DAYS),
-      });
-}
+  res.cookie('refreshToken', session.refreshToken, {
+    httpOnly: true,
+    expires: new Date(Date.now() + THIRTY_DAYS),
+  });
+  res.cookie('sessionId', session._id, {
+    httpOnly: true,
+    expires: new Date(Date.now() + THIRTY_DAYS),
+  });
+};
 
 export const registerUserController = async (req, res) => {
-    const user = await registerUser(req.body);
+  const user = await registerUser(req.body);
 
-    res.status(201).json({
-        status: 201,
-        message: "Successfully registered user",
-        data: user,
-    })
+  res.status(201).json({
+    status: 201,
+    message: 'Successfully registered user',
+    data: user,
+  });
 };
 
 export const loginUserController = async (req, res) => {
-     const session = await loginUser(req.body);
+  const session = await loginUser(req.body);
 
-     setupSession(res, session);
+  setupSession(res, session);
 
-    res.status(200).json({
-        status: 200,
-        message: "Successfully logged in an user!",
-        data:{
-            accessToken: session.accessToken,
-        },
-    })
+  res.status(200).json({
+    status: 200,
+    message: 'Successfully logged in an user!',
+    data: {
+      accessToken: session.accessToken,
+    },
+  });
 };
 
 export const refreshTokenController = async (req, res) => {
-    const {refreshToken, sessionId} = req.cookies;
-    const session = await refreshSession({refreshToken, sessionId});
+  const { refreshToken, sessionId } = req.cookies;
+  const session = await refreshSession({ refreshToken, sessionId });
 
-    setupSession(res, session);
+  setupSession(res, session);
 
-    res.status(200).json({
-        status: 200,
-        message: "Successfully refreshed a session!",
-        data:{
-            accessToken: session.accessToken,
-        },
-    })
-}
+  res.status(200).json({
+    status: 200,
+    message: 'Successfully refreshed a session!',
+    data: {
+      accessToken: session.accessToken,
+    },
+  });
+};
 
 export const logoutUserController = async (req, res) => {
- if(req.cookies.sessionId) {
+  if (req.cookies.sessionId) {
     await logoutUser(req.cookies.sessionId);
- };
+  }
 
   res.clearCookie('sessionId');
   res.clearCookie('refreshToken');
@@ -63,20 +72,45 @@ export const logoutUserController = async (req, res) => {
 };
 
 export const requestResetEmailController = async (req, res) => {
-await requestResetToken(req.body.email);
+  await requestResetToken(req.body.email);
 
-res.json({
+  res.json({
     status: 200,
-    message: "Reset password email has been successfully sent.",
-    data: {}
-})
+    message: 'Reset password email has been successfully sent.',
+    data: {},
+  });
 };
 
 export const resetPasswordController = async (req, res) => {
-    await resetPassword(req.body);
-    res.json({
-        status: 200,
-        message: "Password has been successfully reset.",
-        data: {}
-    })
+  await resetPassword(req.body);
+  res.json({
+    status: 200,
+    message: 'Password has been successfully reset.',
+    data: {},
+  });
+};
+
+export const getGoogleOAuthUrlController = async (req, res) => {
+  const url = generateAuthUrl();
+
+  res.json({
+    status: 200,
+    message: 'Successfully get Google OAuth url!',
+    data: {
+      url,
+    },
+  });
+};
+
+export const loginWithGoogleController = async (req, res) => {
+  const session = await loginOrSignupWithGoogle(req.body.code);
+  setupSession(res, session);
+
+  res.json({
+    status: 200,
+    message: 'Successfully logged in via Google OAuth!',
+    data: {
+      accessToken: session.accessToken,
+    },
+  });
 };
--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ import authRouter from "./routers/auth.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 import { notFoundHandler } from "./middlewares/notFoundHandler.js";
 import { UPLOAD_DIR } from "./constants/index.js";
+import { swaggerDocs } from "./middlewares/swaggerDocs.js";
 
 const port = Number(getEnvVar("PORT", 3000));
 
@@ -17,6 +18,7 @@ export const setupServer = () => {
     app.use(cors());
     app.use(express.json());
     app.use('/uploads', express.static(UPLOAD_DIR));
+    app.use('/api-docs', swaggerDocs());
     app.use(cookieParser());
     app.use(
         pino({
